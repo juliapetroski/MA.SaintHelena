d5fa56e26c1423d30eb1e1fcc9d4c9beb3c14b53
Jack H Laverick from Strath <jacklaverick@ymail.com>
2023-09-04 15:02:34 +0100

    Feat: precipitation and NULL saltless scripts.

--- R scripts/saltless/saltless.04 RIVER VOLUMES.R
+++
@@ -1,8 +0,0 @@
-
-#### Set up ####
-
-rm(list=ls())                                                               # Wipe the brain
-
-packages <- c("tidyverse", "sf", "furrr", "raster", "data.table")           # List packages
-lapply(packages, library, character.only = TRUE)                            # Load packages
-source("./R scripts/@_Region file.R")                                       # Define project region 
